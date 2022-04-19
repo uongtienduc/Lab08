12ce20b3b3664fc5f4e85a04e864eef6fba4f70d
Student <traanfhwng@gmail.com>
2022-04-19 08:23:41 +0700

    update login authenticators

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnregister, btnsignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(MainActivity.this, RegisterScreen.class);
+                startActivity(I);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/RegisterScreen.java
+++ app/src/main/java/com/example/myapplication/RegisterScreen.java
@@ -2,13 +2,45 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterScreen extends AppCompatActivity {
 
+    TextInputLayout regName, regEmail, regPassword, regPassword2;
+    Button regBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+        
+        regName = findViewById(R.id.username);
+        regEmail = findViewById(R.id.email);
+        regPassword = findViewById(R.id.password);
+        regPassword2 = findViewById(R.id.password2);
+        regBtn = findViewById(R.id.button3);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("user");
+
+                reference.setValue("First data storage");
+            }
+        });
+
+
     }
 }
